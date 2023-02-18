c5656c4fce13d7dd02ee3375ac37a25d8ef42edb
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2023-02-18 14:46:57 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const progressBarFull = document.getElementById("progressBarFull");
 const scoreText = document.getElementById("score");
@@ -9,22 +9,24 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
+// Define the questions as an array of objects
+const questions = [
   {
-    question: "  What is the name of the iconic book that contained insults about almost every person in the school?",
+    question:
+      "What is the name of the iconic book that contained insults about almost every person in the school?",
     choice1: "Burn Book",
-    choice2: " The Bible",
+    choice2: "The Bible",
     choice3: "Gossip Lounge",
     choice4: "The Fetch",
-    answer: 1
+    answer: 1,
   },
   {
-    question: "What is the name of the Math Club that Cady was in ?",
+    question: "What is the name of the Math Club that Cady was in?",
     choice1: "Calculus Hero",
     choice2: "Calc-oholics",
     choice3: "Mathletes",
     choice4: "Axis Anything",
-    answer: 3
+    answer: 3,
   },
   {
     question: "Which member was NOT part of the plastics?",
@@ -32,7 +34,7 @@ let questions = [
     choice2: "Regina George",
     choice3: "Karen Smith",
     choice4: "Lizzy Peters",
-    answer: 4
+    answer: 4,
   },
   {
     question: "Who are the first friends that Cady makes on her first day of school?",
@@ -40,14 +42,15 @@ let questions = [
     choice2: "Lizzy Caplan Niel Flynn",
     choice3: "Ely Henry and Daria Malen",
     choice4: "Jane Morrison and Alan Skye",
-    answer: 1
-  }
+    answer: 1,
+  },
 ];
 
 // constants
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+const MAX_QUESTIONS = questions.length;
 
+// Start the game
 startGame = () => {
   questionCounter = 0;
   score = 0;
@@ -56,43 +59,32 @@ startGame = () => {
   getNewQuestion();
 };
 
+// Get a new random question
 getNewQuestion = () => {
-  if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem('mostRecentScore', score);
-    // end the game
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    // End the game
     return window.location.assign("/end.html");
   }
 
   questionCounter++;
-  progressText.innerText = `Question ${ questionCounter}  / ${ MAX_QUESTIONS}`;
-//Update the progress bar//
+  progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
 
-progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
+  // Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = currentQuestion.question;
 
-  choices.forEach(choice => {
-    const number = choice.dataset.number;
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
 
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswer = true;
 };
-
-
-choices.forEach(choice => {
-  choice.addEventListener('click', e => {
-    handleChoiceSelection(e.target);
-  });
-
-   choice.addEventListener('touchstart', e => {
-    handleChoiceSelection(e.target);
-  });
-}); 
     
     
     
